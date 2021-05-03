0c8cbc2999e4209c95fa7400eeb9c7c03ff252b4
Ms-Alex <ms.hernandeza1@gmail.com>
2021-05-03 02:01:19 -0400

    added navbar and updated to use custom toggler

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,26 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">AH</a>
+
+                <button className="navbar-toggler " type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-end" id="navbarToggler">
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Experience</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
